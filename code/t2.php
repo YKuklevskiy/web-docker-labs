b81a5259862485e467c411021ab6eff0b04af77e
Yaroslav <ykuklevskiy@gmail.com>
2022-02-05 17:42:20 +0200

    Lab PHP task 2

---
+++ code/t2.php
@@ -0,0 +1,13 @@
+<?php
+
+$num1 = 7; // integer
+echo $num1, "\n";
+
+$num2 = 11.1; // floating-point
+echo $num2, "\n";
+
+echo 8+4, "\n"; // 12
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month;
